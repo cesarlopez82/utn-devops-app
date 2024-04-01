1a1b7bd86a017e7c459acc5f045d3b9fc089d3e0
CesarLopez <clopez@sumtechlabs.com>
2024-03-31 21:52:25 -0300

    agrego endpoints y coneccion a db

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Bienvenido al home page!'
+# Configurar la conexión a la base de datos
+db_connection = mysql.connector.connect(
+    host="db_server",
+    user="root",
+    password="root",
+    database="devops_app"
+)
+db_cursor = db_connection.cursor()
 
-@app.route('/test')
-def test():
-    return 'Este es el endpoint /test modificado'
+# Endpoint para consultar la base de datos y obtener el resultado como JSON
+@app.route('/prueba1')
+def prueba1():
+    query = "SELECT * FROM welcome WHERE description = 'prueba 1'"
+    db_cursor.execute(query)
+    result = db_cursor.fetchall()
+    return jsonify(result)
+
+# Endpoint para consultar la base de datos y obtener el resultado como JSON
+@app.route('/prueba2')
+def prueba2():
+    query = "SELECT * FROM welcome WHERE description = 'prueba 2'"
+    db_cursor.execute(query)
+    result = db_cursor.fetchall()
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
